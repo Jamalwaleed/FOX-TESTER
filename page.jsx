be373dc99c30a18db76fd8b73fffe9f6f5acd300
Jamalwaleed <jaamalwaleeed@gmail.com>
2025-07-09 03:44:09 +0300

    Add files via upload

---
+++ page.jsx
@@ -0,0 +1,7 @@
+"export default function Home() {"
+"  return ("
+"    <main style={{ minHeight: '100vh', padding: '2rem', backgroundColor: '#f3f4f6' }}>"
+"      <h1 style={{ color: 'blue', textAlign: 'center' }}>FOX TESTER ???? ???????!</h1>"
+"    </main>"
+"  )"
+"}"
